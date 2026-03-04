a876848e5107a0e97a3a1e1459e27f59f80f0a66
Leah Green <159081746+leahgreen02@users.noreply.github.com>
2026-03-04 15:22:52 -0800

    typo

--- bilby/gw/eos/eos.py
+++ bilby/gw/eos/eos.py
@@ -1277,7 +1277,7 @@ def ChebyshevNeutronStarEOSSpectralDecomposition(upsilons):
     print("shape", eos_table.shape)
     # XLALSimNeutronStarEOSFromArray
     #integer = 29
-    eos = lalsim.SimNeutronStarEOSFromArray(eos_table, n_col, "chebyshev_spectral")
+    eos = lalsim.SimNeutronStarEOSFromArray(eos_table, ncol, "chebyshev_spectral")
     print("eos", eos)
 
     return eos 
